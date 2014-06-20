9798133b794c7770d317a433827d4f7ab6949e4c
Luka <luka.toni93@gmail.com>
2014-06-20 16:36:45 +0200

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -1,2 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int maxBlock(int** z, int m, int n)
+{
+  int max = 0;
+  int i,j;
+  int k,l;
+  int inBox = 0;
+  
+  for (i = 0; i < m ; i++)
+  {
+    for(j = 0; j < n; j++)
+    {
+      if(z[i][j] == 1)
+      {
+        max = 1;
+        
+        for(k=j; k < n ; k++)
+          {
+            if(z[i][j] == 1)
+              inBox++;
+            else
+              break;
+          }
+          
+        if(inBox == 1)
+        {
+          if(max < inBox)
+            max = inBox;
+        }
+        
+        if(inBox != 1 && inBox != 0)
+        {
+          
+          
+        }
+        
+        
+      }
+      else
+        continue;
+    }
+  }  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+int main ()
+{
+  int matrix[5][6] = {
+    {0,1,0,1,0,0},
+    {0,1,1,1,1,0},
+    {0,1,1,1,1,0},
+    {0,0,1,1,1,1},
+    {1,0,0,1,0,1}
+    }; 
+  
+  printf("%d\n", maxBlock(matrix, 5, 6)); 
+  return 0;
+}
