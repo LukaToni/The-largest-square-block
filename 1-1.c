1c2c833282f5435b444f9c6c49c7995932fff6a7
Luka <luka.toni93@gmail.com>
2017-02-24 14:14:25 +0100

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -1,71 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int maxBlock(int** z, int m, int n)
-{
+int maxBlock(int** z, int m, int n) {
   int max = 0;
   int i,j;
   int k,l;
   int inBox = 0;
   
-  for (i = 0; i < m ; i++)
-  {
-    for(j = 0; j < n; j++)
-    {
-      if(z[i][j] == 1)
-      {
+  for(i = 0; i < m ; i++) {
+    for(j = 0; j < n; j++) {
+      if(z[i][j] == 1) {
         max = 1;
-        
-        for(k=j; k < n ; k++)
-          {
-            if(z[i][j] == 1)
+        for(k=j; k < n ; k++) {
+            if(z[i][j] == 1) {
               inBox++;
-            else
+            }
+            else {
               break;
-          }
+            }
+        }
           
-        if(inBox == 1)
-        {
-          if(max < inBox)
+        if(inBox == 1) {
+          if(max < inBox) {
             max = inBox;
+          }
         }
         
-        if(inBox != 1 && inBox != 0)
-        {
+        if(inBox != 1 && inBox != 0) {
           int to = k + inBox;
-          for(k = j ; k < to; k++)
-          {
-            for(l = i; l < to; l++)
-            {
-              if(z[k][l]!=1)
-              {
-                if(k - i == l - j)
+          for(k = j ; k < to; k++) {
+            for(l = i; l < to; l++) {
+              if(z[k][l]!=1) {
+                if(k - i == l - j) {
                   max = k - i - 1;
-                if(k - i < l - j)
+                }
+                if(k - i < l - j) {
                   max = k - i;
-                else
+                }
+                else {
                   max = l - j;
-                goto jump ;
+                }
+                break;
               }
-              else
-                continue;
             }
           }
         }
       }
-      jump:
-      else
-        continue;
     }
-    
   }  
-  
   return max;
-  
 }
 
-int main ()
-{
+int main () {
   int matrix[5][6] = {
     {0,1,0,1,0,0},
     {0,1,1,1,1,0},
